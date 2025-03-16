505a9d2ba6ad00a9b40d2c09a52739cb762835f5
taufikmuharrom <taufikmuharromdev@gmail.com>
2025-03-16 13:36:15 +0700

    update

--- src/layout/HeaderLayout.vue
+++ src/layout/HeaderLayout.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>HEADER</div>
+  <header>
+    <section id="header">
+      <div class="header container">
+        <nav>
+          <div class="nav-bar">
+            <div class="logo">
+              <a href=""><img src="../assets/logo.svg" /></a>
+            </div>
+            <div class="nav-list">
+              <div class="hamburger">
+                <div class="bar"></div>
+              </div>
+              <ul class="nav-item">
+                <li><a href="./about.html">about</a></li>
+                <li><a href="./location.html">Location</a></li>
+                <li><a href="./careers.html">Careers</a></li>
+                <a class="btn" href="#">Get Scootin</a>
+              </ul>
+            </div>
+            <div class="nav-btn">
+              <a class="btn" href="">Get Scootin</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </section>
+  </header>
 </template>
 
 <script lang="ts" setup></script>
 
-<style></style>
+<style scoped></style>
